0749ef2d618652ab6c563cc43a9f0a6694362979
Leo Hinterlang <leohinterlang@gmail.com>
2014-07-22 00:26:57 -0400

    Fix bugs in Debug. Update README.

--- src/main/java/com/fidelis/util/Debug.java
+++ src/main/java/com/fidelis/util/Debug.java
@@ -1,52 +1,52 @@
 /**
  *	Debug.java
  */
-package com.fidelis.util
+package com.fidelis.util;
 
 public class Debug {
-    private static boolean verbose;
-    private static boolean trace;
+	private static boolean verbose;
+	private static boolean trace;
 
-    /**
-     * Sets the verbose operating mode.
-     * The value {@code true} enables verbose mode while the value 
-     * {@code false} disables it.
-     *
-     * @param verbose {@code true} enables verbose mode
-     */
-    public static void setVerbose (boolean verbose) {
-    	this.verbose = verbose;
-    }
-
-    /**
-     * Returns the state of the verbose operating mode.
-     * If verbose mode is enabled, the value {@code true} is returned while
-     * the value {@code false} indicates that verbose mode is disabled.
-     *
-     * @return {@code true} if verbose mode is enabled
-     */
-    public static boolean isVerbose () {
-    	return verbose;
-    }
-
-    public static void setTrace (boolean trace) {
-    	this.trace = trace;
-    }
-
-    public static boolean isTrace () {
-    	return trace;
-    }
-
-    public static void verbose (String text) {
-    	if (verbose) {
-    	    System.out.println(text);
+	/**
+	 * Sets the verbose operating mode. The value {@code true} enables verbose
+	 * mode while the value {@code false} disables it.
+	 * 
+	 * @param mode
+	 *            {@code true} enables verbose mode
+	 */
+	public static void setVerbose (boolean mode) {
+		verbose = mode;
 	}
-    }
 
-    public static void trace (String text) {
-    	if (trace) {
-    	    System.out.println(text);
+	/**
+	 * Returns the state of the verbose operating mode. If verbose mode is
+	 * enabled, the value {@code true} is returned while the value {@code false}
+	 * indicates that verbose mode is disabled.
+	 * 
+	 * @return {@code true} if verbose mode is enabled
+	 */
+	public static boolean isVerbose () {
+		return verbose;
 	}
-    }
+
+	public static void setTrace (boolean mode) {
+		trace = mode;
+	}
+
+	public static boolean isTrace () {
+		return trace;
+	}
+
+	public static void verbose (String text) {
+		if (verbose) {
+			System.out.println(text);
+		}
+	}
+
+	public static void trace (String text) {
+		if (trace) {
+			System.out.println(text);
+		}
+	}
 
 }
